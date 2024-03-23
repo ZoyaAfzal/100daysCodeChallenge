0b33d1f9b62c44082b3c29fd732b346b6aa0fa40
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-03-24 00:45:20 +0500

    100daysCodeChallenge

---
+++ Day6/Q64.ts
@@ -0,0 +1,36 @@
+/*Question 64: Combine Strings and Numbers: Write a function that merges a piece of text with a number. For example,
+if provided with "Age: " and 30, it should give back "Age: 30".*/
+
+//This function merges a test and a number:
+
+function combineStringAndNumber(text: string, number: number): string {
+    //Joining the text and and number into single text:
+    return text + number;
+
+};
+//Trying it out with "Age: " and 30:
+console.log(combineStringAndNumber("Age: " , 30));
+//Here, we put together the text and a number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
