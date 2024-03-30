d22e309dbfea2d59f0d6fb442de64f2e0d7646bd
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-03-30 23:49:49 +0500

    100daysCodeChallenge

---
+++ Day9/Q79.js
@@ -0,0 +1,16 @@
+/*Question 79: Creating and Accessing Object Properties: Construct an object representing a car with properties
+for make, model, and year. Then, show how you can access the model property of the car.*/
+//This sets up an Object for a car with specific details:
+let car = {
+    make: "Ferrari",
+    model: "SF90 Stradale",
+    year: 2019,
+};
+//Accessing and showing the car make:
+console.log(car.make);
+//Accessing and showing the car model:
+console.log(car.model);
+//Accessing and sharing the car year:
+console.log(car.year);
+export {};
+//We use dot notation to get the property of an Object.
---
+++ Day9/Q80.js
@@ -0,0 +1,17 @@
+/*Question 80: Updating Object Properties: Add a property named color to the existing car object, and then update
+the year property to 2021. Show how to perform these operations.*/
+//Starting with our car object:
+let car = {
+    make: "Ferrari",
+    model: "SF90 Stradale",
+    year: 2019,
+};
+//Adding a new property 'color' and updating 'year':
+car.color = "Red";
+//Added a new property color:
+car.year = 2021;
+//Updated the year property
+//Showing the updated Car Object:
+console.log(car);
+export {};
+//Now, our car is Red and the year has been updated to 2021.
