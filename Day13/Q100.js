548073b3acf90df7445088d10e56fcb82e3c1bb0
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-04-04 06:07:10 +0500

    1oodaysCodeChallenge

---
+++ Day13/Q100.js
@@ -0,0 +1,6 @@
+//Question 100: Use the JavaScript Math object to find the square root of 144.
+//Find the square root of 144 using Math.sqrt():
+const squareRoot = Math.sqrt(144);
+console.log(squareRoot);
+export {};
+// This line effectively calculates and shows the square root of 144.
---
+++ Day13/Q102.js
@@ -0,0 +1,6 @@
+//Question 102: Calculate and log the absolute difference between the number -5 and 5.
+//Calculating the absolute difference between the number -5 and 5:
+const difference = Math.abs(-5 - 5);
+console.log(difference);
+export {};
+//This line shows the absolute value of the difference between -5 and 5, ignoring the sign.
