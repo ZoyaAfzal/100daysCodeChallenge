81d275071ffde57ae5531db7e6c3d8444b5fd2a7
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-04-15 01:30:06 +0500

    100daysCodeChallenge

---
+++ Day22/Q133.ts
@@ -0,0 +1,17 @@
+//Question 134: Take a JSON string and parse it into a JavaScript object.
+
+//Defining a JavaScript Object:
+const person = {
+    name: "Charles",
+    age: 25,
+    city: "Austin"
+};
+//Converting the object into a JSON string:
+const jsonString = JSON.stringify(person);
+//Outputs:{"name":"Charles","age":25,"city":"Austin"}
+
+
+//Logging the json string:
+console.log(jsonString);
+
+//Demonstrated converting an object to a JSON string, making it easy to store or transmit.
---
+++ Day22/Q135.js
@@ -0,0 +1,20 @@
+//Question 135: Explain how you can format a JSON string with proper indentation for readability.
+//Reusing the 'person' object from Question 133:
+const person = {
+    name: "Charles",
+    age: 25,
+    city: "Austin"
+};
+//Converting the object into a JSON string with identation:
+const jsonString = JSON.stringify(person, null, 2);
+//The '2' specifies the number of spaces to use as white space.
+//Logging the json string:
+console.log(jsonString);
+export {};
+/*Output:
+{
+    name: "Charles",
+    age: 25,
+    city: "Austin"
+}; */
+// Shows how adding indentation to the JSON string makes it easier to read.
