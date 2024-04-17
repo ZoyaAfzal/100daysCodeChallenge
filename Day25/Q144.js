6d1a5767dd0e15e7ac24d1dd2fac8b59285d2b1f
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-04-18 03:59:18 +0500

    100daysCodeChallenge

---
+++ Day25/Q144.js
@@ -0,0 +1,15 @@
+//Question 144: Explain the use of the Promise.all() method with an example.
+// Example using Promise.all to wait for multiple promises to resolve
+const promise1 = Promise.resolve(5);
+const promise2 = 56;
+const promise3 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Promise");
+    }, 2000);
+});
+Promise.all([promise1, promise2, promise3]).then((values) => {
+    console.log(values);
+    //outputs:[ 5, 56, 'foo' ]
+});
+export {};
+// This demonstrates how Promise.all waits for all promises to resolve and then logs the array of their results.
