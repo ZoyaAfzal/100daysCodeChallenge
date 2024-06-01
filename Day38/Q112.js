1face5be94884868bc7c33a69a76e87b8395f5d3
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-02 03:01:14 +0500

    100daysCodeChallenge

---
+++ Day38/Q112.js
@@ -0,0 +1,19 @@
+/*Question 112: Create a Map that stores the names of countries as keys and their capitals as values. Add three
+countries to the Map.*/
+//Creating a new Map to store Countries and their Capitals:
+const countries = new Map();
+countries.set("USA", "Washington, D.C.");
+//Added USA to the Map
+countries.set("France", "Pris");
+//Added France to the Map
+countries.set("Japan", "Tokyo");
+//Added Japan to the Map
+countries.set("Germany", "Berlin");
+//Added Germany to the Map
+countries.set("Malaysia", "Kaula Lumpur");
+//Added Malaysia to the Map
+console.log("Indonesia", "Jakarta");
+//Added Indonesia to the Map
+console.log(countries);
+export {};
+//Logging the map with the countries and their capitals.
