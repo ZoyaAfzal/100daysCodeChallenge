da28ed670c95456a59e663fb0d38f69cbca7d1dc
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-02 02:11:05 +0500

    100daysCodeChallenge

--- Day37/Q103.ts
+++ Day37/Q103.ts
@@ -1,6 +1,6 @@
 //Question 103: Write a function that returns a random boolean value, true or false.
 
-//This function returns a random boolean value:
+//This function returns a random boolean value :
 function getRandomBooleanValue():boolean{
     return Math.random() > 0.5;
     //It will return true if the random number is greater than 0.5
