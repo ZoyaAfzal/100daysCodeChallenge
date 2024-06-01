052785c5a7f2347476bbb984d7be2b70f3a89a6f
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-01 20:39:49 +0500

    100daysCodeChallenge

---
+++ Day45/Q134.ts
@@ -0,0 +1,14 @@
+//Question 134: Take a JSON string and parse it into a JavaScript object.
+
+//Defining a Json String:
+const jsonString = `{"name":"Charles","age":25,"city":"Austin"}`;
+
+//Passing the JSON string back into a JavaScript object:
+const person = JSON.parse(jsonString);
+
+//logging the JavaScript object:
+console.log(person);
+//Outputs will be the Original object.
+
+// This snippet shows how to take a JSON string and convert it back into a JavaScript object.
+
