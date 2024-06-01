442555913578575dc9a8f126164dfaf3299bddcf
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-02 03:10:32 +0500

    100daysCodeChallenge

---
+++ Day35/Q105.js
@@ -0,0 +1,11 @@
+//Question 105: Simulate a dice roll using JavaScript and return a random integer between 1 and 6.
+//This function simulates rolling a dice and return a number between i and 6:
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+    //Calculating a random integer from 1 to 6
+}
+;
+console.log(rollDice());
+export {};
+//Output will be a random number between 1 and 6
+//Here, we mimic the action of rolling a dice and getting a result.
