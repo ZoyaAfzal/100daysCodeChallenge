0994f86f61a89f848fc9cbdbb6d966407e1160ae
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-02 03:12:46 +0500

    100daysCodeChallenge

--- Day34/Q91.js
+++
@@ -1,8 +0,0 @@
-//Question 91: Create an array of your three favorite fruits and add a new fruit to the end of the array.
-//Defining an array with my three most favorite fruits:
-let favoriteFruits = ["Strawberries", "Kiwies", "Cherries"];
-favoriteFruits.push("PineApples");
-//Add "PineAplles" to the end of the array
-console.log(favoriteFruits);
-export {};
-//It will add a new fruit to our favoriteFruits list.
