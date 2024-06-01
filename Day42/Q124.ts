65e81611508ab9551d2e3cc1b0163ea5a68d62a0
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-02 02:34:25 +0500

    100daysCodeChallenge

---
+++ Day42/Q124.ts
@@ -0,0 +1,12 @@
+//Question 124: Create a function inside an object that returns the object's own name property using the this keyword.
+
+//Defining an object with a name property and a method to return it's name:
+const person = {
+    name: "Steven",
+    getName: function(){
+        return this.name;
+        //Used 'this' to refer to the object itself and return its 'name' property
+    },
+};
+console.log(person.getName());
+//This method correctly identifies and return the object's 'name' property using 'this'.
