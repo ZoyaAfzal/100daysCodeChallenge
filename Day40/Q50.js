fc844bcdc1c483c7100f22619f0c4d5f2fb277f3
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-01 21:18:35 +0500

    100daysCodeChallenge

--- Day40/Q50.js
+++
@@ -1,4 +0,0 @@
-/*Question 50: Multiline Template Literals: Use template literals to create a multiline string that describes your
-ideal day. Include at least three different activities.*/
-var myIdealDay = "My Ideal Day:\n1-Start my day grabbing a cup of coffee or tea and need some time for peaceful meditation.\n2-In afternoon, having lunch with family and spending quality time with them.\n3-Then i keep myself busy in doing coding or reading.\n4-I try to make the most of my day by fulfilling all the tasks i scheduled.\n5-End my day think about the things that can help me become more organized with my works.";
-console.log(myIdealDay);
