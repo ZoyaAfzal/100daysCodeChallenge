cd73602ec540edc3dcc5361175c29ba439399535
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-02 01:50:37 +0500

    1oodaysCodeChallenge

---
+++ Day36/Q100.ts
@@ -0,0 +1,7 @@
+//Question 100: Use the JavaScript Math object to find the square root of 144.
+
+//Find the square root of 144 using Math.sqrt():
+const squareRoot:number = Math.sqrt(144);
+
+console.log(squareRoot);
+// This line effectively calculates and shows the square root of 144.
