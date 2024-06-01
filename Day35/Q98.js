3cbf6780b76fbcd34eabd29ad8e92ada6e360738
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-02 01:52:26 +0500

    1oodaysCodeChallenge

---
+++ Day35/Q98.js
@@ -0,0 +1,15 @@
+//Question 98: Create a JavaScript snippet that calculates and logs how many days are left until New Year.
+//Calculating how many days are lef tuntil new Year's Day:
+function daysUntilNewYear() {
+    const today = new Date();
+    const newYear = new Date(today.getFullYear() + 1, 0, 1);
+    //January 1st of next year
+    const diff = newYear.getTime() - today.getTime();
+    //Difference in milliseconds
+    const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+    //Converting the days
+    return days;
+}
+console.log(daysUntilNewYear() + "days until New Year");
+export {};
+//Telling us how many days are until the next New Year's Day.
---
+++ Day35/Q99.js
@@ -0,0 +1,19 @@
+//Question 99: Generate a date object representing your next birthday and log it to the console.
+//Generating a Date Object for the next occurence of a specific birthday:
+function getNextBirthday(month, day) {
+    const today = new Date();
+    let year = today.getFullYear();
+    const birthday = new Date(year, month - 1, day);
+    //Months are 0-indexed
+    if (birthday < today) {
+        //if the birthday this year already passed, use next year's date
+        birthday.setFullYear(year + 1);
+    }
+    return birthday;
+}
+//Replace with your birth and month:
+const nextBirthday = getNextBirthday(8, 11);
+//Example: August 11
+console.log("Next Birthday is on:", nextBirthday.toLocaleDateString());
+export {};
+//Shows the date of the next birthday.
