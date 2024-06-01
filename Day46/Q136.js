d9e670c42bd948304ec7b02df3c8f744411c74ee
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-01 20:36:29 +0500

    100daysCodeChallenge

---
+++ Day46/Q136.js
@@ -0,0 +1,9 @@
+//Question 136: Use console.log() to debug and track the value of a variable inside a loop.
+//Demonstrating using console.log() inside a loop to track variable values:
+for (let i = 0; i <= 5; i++) {
+    console.log(`Iteration ${i} , i value:`, i);
+    //Logging the current iteration number and the value of i
+}
+;
+export {};
+//This loop will iterate five times, logging the value of 'i' during each iteration to help with debugging.
