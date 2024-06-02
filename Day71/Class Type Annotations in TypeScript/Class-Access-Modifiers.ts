cfe982460ad2fd003052bfb2b27f12bd78bf32c6
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-02 18:26:59 +0500

    100daysCodeChallenge

---
+++ Day71/Class Type Annotations in TypeScript/Class-Access-Modifiers.ts
@@ -0,0 +1,52 @@
+//Class-Access-Modifiers :
+
+class MyClassPublic {
+    public name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+const instancePublic = new MyClassPublic("John");
+console.log(instancePublic.name); // Accessing the public property
+
+
+class MyClassPrivate {
+    private secret: string;
+
+    constructor(secret: string) {
+        this.secret = secret;
+    }
+
+    revealSecret() {
+        console.log(this.secret); // Accessing the private property from within the class
+    }
+}
+
+const instancePrivate = new MyClassPrivate("My secret");
+//console.log(instancePrivate.secret); // This would result in an error because secret is private
+instancePrivate.revealSecret(); // This is a valid way to access the private property
+
+
+class Parent {
+    protected familyName: string;
+
+    constructor(name: string) {
+        this.familyName = name;
+    }
+}
+
+class Child extends Parent {
+    introduceFamily() {
+        console.log(`Our family name is ${this.familyName}`);
+    }
+}
+
+const parentEx = new Parent("Smith");
+const childEx = new Child("Johnson");
+
+//console.log(parentEx.familyName); // This would result in an error because familyName is protected
+childEx.introduceFamily(); // This is a valid way to access the protected property
+
+
---
+++ Day71/Class Type Annotations in TypeScript/Class-Type-Annotations.js
@@ -0,0 +1,23 @@
+//Class-Type-Annotations :
+class Product {
+    // Property type annotations
+    id;
+    name;
+    price;
+    constructor(id, name, price) {
+        // Constructor parameter type annotations
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    // Method type annotations
+    getProductInfo() {
+        return `ID: ${this.id}, Name: ${this.name}, Price: $${this.price}`;
+    }
+}
+// Create an instance of the Product class
+const product1 = new Product(1, "Widget", 20.0);
+// Access class properties and call a method
+console.log(product1.getProductInfo());
+export {};
+// Output: ID: 1, Name: Widget, Price: $20
