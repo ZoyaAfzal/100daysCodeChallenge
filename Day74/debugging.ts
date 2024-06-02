b153ab376c72208b96ef2ec3aa88d2783ae856a2
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-02 23:47:59 +0500

    100daysCodeChallenge

---
+++ Day74/debugging.ts
@@ -0,0 +1,12 @@
+//Debugging :
+
+let age: number = 22;
+if (age > 20) {
+    console.log("pass");
+
+} else if (age < 20) {
+    console.log("return");
+    
+} else {
+    console.log("reboot");
+}
