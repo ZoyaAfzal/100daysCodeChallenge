05f3ba93b7a0e220f401b797211e02cd0db42062
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-04 17:10:53 +0500

    100daysCodeChallenge

---
+++ Day11/Q31.js
@@ -0,0 +1,11 @@
+//Question 31: No Users: Ensure your user list isn’t empty.
+//Explain & TIP: Before greeting users, check if there are any. This teaches you to handle empty arrays.
+let usernames = [];
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    // Greet users
+}
+export {};
+// Removing all usernames ensures the message "We need to find some users!" is printed.
--- Day11/Q91.ts
+++
@@ -1,9 +0,0 @@
-//Question 91: Create an array of your three favorite fruits and add a new fruit to the end of the array.
-
-//Defining an array with my three most favorite fruits:
-let favoriteFruits:string[] = ["Strawberries", "Kiwies", "Cherries"];
-favoriteFruits.push("PineApples");
-//Add "PineAplles" to the end of the array
-
-console.log(favoriteFruits);
-//It will add a new fruit to our favoriteFruits list.
