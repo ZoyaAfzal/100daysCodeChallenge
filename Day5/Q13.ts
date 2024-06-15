acf3240fccdd2316eb14d6555041833a84ce4f7a
Zoya Afzal <160391124+ZoyaAfzal@users.noreply.github.com>
2024-06-15 19:18:33 +0500

    100daysCodeChallenge

---
+++ Day5/Q13.ts
@@ -0,0 +1,10 @@
+/*Question-13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car,
+and make a list that stores several examples. Use your list to print a series of statements about these items, 
+such as “I would like to own a Honda motorcycle.”*/
+
+let vehicles : String[] = ["Ferrari296GTB", "BMW X6" ,"Audi Rs e-tron GT", "Rolls-Royce 2023"];
+
+vehicles.map((items) => console.log(`${items} are the most luxurious cars.`));
+vehicles.map((items) => console.log(`I wish i can buy ${items}.`));
+vehicles.map((items) => console.log(`My most favourite car is ${items}.`));
+vehicles.map((items) => console.log(`I would like to own  ${items}.`));
